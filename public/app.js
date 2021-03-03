8dc1cf15eab537bf321457a7f30f4abb9405e68a
aofodile <70602087+aofodile@users.noreply.github.com>
2021-03-03 15:38:11 -0500

    push

--- public/app.js
+++ public/app.js
@@ -1,11 +1,10 @@
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
 
 const zips = [];
-// let response = await fetch(endpoint)
-// let data = await zips.push(data)
+
 fetch(endpoint)
- .then(blob => blob.json())
- .then(data => zips.push(data));
+	.then(blob => blob.json())
+ 	.then(data => zips.push(...data));
 
 function findMatches(wordToMatch, zips) {
 	return zips.filter(restr => {
@@ -14,23 +13,16 @@ function findMatches(wordToMatch, zips) {
   	});
 }
 
-function numberWithCommas(x) {
-	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
-
 function displayMatches() {
+	// console.log(this.value)
   	const matchArray = findMatches(this.value, zips);
   	const html = matchArray.map(restr => {
-    	const regex = new RegExp(this.value, 'gi');
-    	const zipNo = restr.zip.replace(regex, `<span class="hl">${this.value}</span>`);
-    	return `
-    		<li>
-    	    	<span class="name">${zipNo}</span>
-      		</li>
-    	`;
-  	}).join('');
+  		return `<li><span class="population">${restr.name}</span><span class="name">${restr.zip}</span></li>`;
+  	}).join('')
   	suggestions.innerHTML = html;
+  	
 }
+
 
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
